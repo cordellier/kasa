f168b904c036a4da8dffe311c834de3ebbcdfbbd
Guillaume <101353118+cordellier@users.noreply.github.com>
2024-07-10 18:19:32 +0200

    ajout

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import Banner from "../components/Banner";
 import Layout from "../components/Layout";
 import backgroundImage from "./../images/banner2.png";
+import About from "../components/About";
 
 const AboutPage = () => {
   return (
     <Layout>
       <Banner backgroundImage={backgroundImage} classStyle="banner2" />
+      <About />
     </Layout>
   );
 };
