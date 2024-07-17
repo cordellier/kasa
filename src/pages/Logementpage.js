b017cfc110525285d1fa3de2eba94eed58b24235
Guillaume <101353118+cordellier@users.noreply.github.com>
2024-07-17 10:37:12 +0200

    syntaxe
    Suppression des chevrons

--- src/pages/Logementpage.js
+++ src/pages/Logementpage.js
@@ -4,11 +4,9 @@ import Layout from "../components/Layout";
 
 const LogementPage = () => {
   return (
-    <>
-      <Layout>
-        <LogementsPage />
-      </Layout>
-    </>
+    <Layout>
+      <LogementsPage />
+    </Layout>
   );
 };
 
