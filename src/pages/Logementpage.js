d5c07d625444fa453329c065096ecac4b3fbe9fc
Guillaume <101353118+cordellier@users.noreply.github.com>
2024-07-17 10:34:57 +0200

    Suppression de la répétivité
    - logementPage répété header et footer
    - Renommé clairement les fichiers.

--- src/pages/Logementpage.js
+++ src/pages/Logementpage.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import LogementsPage from "./../components/LogementsPage";
 import Layout from "../components/Layout";
 
@@ -8,10 +6,8 @@ const LogementPage = () => {
   return (
     <>
       <Layout>
-        <Header />
         <LogementsPage />
       </Layout>
-      <Footer />
     </>
   );
 };
